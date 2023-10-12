4271d16e87fe450c1fe69103c103dde8c0a121dd
Jorge Lucas <lukasrochaaraujo@hotmail.com>
2023-10-12 13:56:00 -0300

    feat: add swagger ui

---
+++ src/main/java/com/template/api/configurations/WebMcvConfiguration.java
@@ -0,0 +1,17 @@
+package com.template.api.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMcvConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD");
+    }
+}
